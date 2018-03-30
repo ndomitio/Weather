15e35f7c5d61066f50717af60f870a246bc9af6b
NicholasDomitio <nicholas.domitio@gmail.com>
2018-03-30 10:35:31 -0400

    first

---
+++ Weather/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weather.Models;
+
+namespace Weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+
+        public JsonResult GetWeather()
+        {
+            WeatherClass weath = new WeatherClass ();
+
+            return Json(weath.getWeatherForecast(),JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetForecast()
+        {
+            WeatherClass forecast = new WeatherClass();
+
+            return Json(forecast.getThreeDay(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Weather/Models/WeatherClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace Weather.Models
+{
+    public class WeatherClass
+    {
+        public Object getWeatherForecast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=01173c00c49e46d41a2f0e4454a42705&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+
+        public Object getThreeDay()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/forecast?zip=44143,us&APPID=01173c00c49e46d41a2f0e4454a42705&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
